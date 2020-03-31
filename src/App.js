a59c57e2e5e12eeb9cb90b0f852abcab9f2ff692
Nghia <nphan@fandm.edu>
2020-03-31 11:26:28 -0500

    login form can be toggled shown or hidden

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import JournalEntry from './components/JournalEntry';
 import Notification from './components/Notification';
+import LoginForm from './components/LoginForm'
 import journalEntryService from './services/journalEntryService'
 import loginService from './services/loginService';
 
@@ -27,6 +28,7 @@ const App = () => {
   const [username, setUsername] = useState('')
 	const [password, setPassword] = useState('')
 	const [user, setUser] = useState(null)
+	const [loginVisible, setLoginVisible] = useState(false)
 
   useEffect(() => {
     journalEntryService
@@ -123,30 +125,28 @@ const App = () => {
 		window.localStorage.removeItem('loggedInUser')
 	}
 	
-	const loginForm = () => (
-		<form onSubmit={handleLogin}>
-			<h2>Login</h2>
+	const loginForm = () => {
+		const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+		const showWhenVisible = { display: loginVisible ? '' : 'none'}
+
+		return (
 			<div>
-				username
-					<input
-					type="text"
-					value={username}
-					name="Username"
-					onChange={({ target }) => setUsername(target.value)}
-				/>
+				<div style={hideWhenVisible}>
+					<button onClick={() => setLoginVisible(true)}>log in</button>
+				</div>
+				<div style={showWhenVisible}>
+					<LoginForm
+						username={username}
+						password={password}
+						handleUsernameChange={({ target}) => setUsername(target.value)}
+						handlePasswordChange={({ target }) => setPassword(target.value)}
+						handleSubmit={handleLogin}
+					/>
+					<button onClick={() => setLoginVisible(false)}>cancel</button>
+				</div>
 			</div>
-			<div>
-				password
-					<input
-					type="password"
-					value={password}
-					name="Password"
-					onChange={({ target }) => setPassword(target.value)}
-				/>
-			</div>
-			<button type="submit">login</button>
-		</form>
-	)
+		)
+	}
 
 	const addJournalEntryForm = () => (
 		<form onSubmit={addEntry}>
